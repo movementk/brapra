3d7f6e1c1223d97a146c618ca06e2b61cce23ac5
mk-kimjh <mk.kimjh@gmail.com>
2016-09-13 20:43:18 +0900

    서브가이드 작성 완료

---
+++ inc/footer.php
@@ -0,0 +1,137 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-5">
+                <section class="cs row">
+                    <div class="col-xs-12">
+                        <h4>CS CENTER</h4>
+                    </div>
+                    <div class="col-xs-6 info">
+                        <strong><img src="/design/brapra/images/common/cs_contact.gif" alt="1544-6101"></strong>
+                        <dl>
+                            <dt>평일</dt>
+                            <dd>10:00~17:00</dd>
+                            <dt>점심시간</dt>
+                            <dd>11:30~13:00</dd>
+                        </dl>
+                        <p>주말/공휴일은 휴무입니다.</p>
+                    </div>
+                    <div class="col-xs-6 quick">
+                        <ul>
+                            <li><a href="#">베스트 리뷰</a></li>
+                            <li><a href="#">회원등급혜택</a></li>
+                            <li><a href="#">자주 묻는 질문</a></li>
+                            <li><a href="#">상품 Q&amp;A</a></li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+            <div class="col-xs-3">
+                <div class="qr">
+                    <ul>
+                        <li>
+                            <img src="/design/brapra/images/common/qr_ios.gif" alt="iOS QR Code">
+                            <p>IOS APP</p>
+                        </li>
+                        <li>
+                            <img src="/design/brapra/images/common/qr_android.gif" alt="Android QR Code">
+                            <p>ANDROID APP</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-4">
+                <section class="notice">
+                    <h4>NOTICE</h4>
+                    <ol>
+                        <li>
+                            <p><a href="#">2016년 5월 황금연휴 휴무안내 2016년 5월 황금연휴 휴무안내 2016년 5월 황금연휴 휴무안내</a></p>
+                            <span>16-08-09</span>
+                        </li>
+                        <li>
+                            <p><a href="#">2016년 5월 황금연휴 휴무안내</a></p>
+                            <span>16-08-09</span>
+                        </li>
+                        <li>
+                            <p><a href="#">2016년 5월 황금연휴 휴무안내</a></p>
+                            <span>16-08-09</span>
+                        </li>
+                        <li>
+                            <p><a href="#">2016년 5월 황금연휴 휴무안내</a></p>
+                            <span>16-08-09</span>
+                        </li>
+                    </ol>
+                    <a class="more" href="#"><span class="sr-only">더보기</span></a>
+                </section>
+            </div>
+        </div>
+    </div>
+    <nav class="foot-nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-4">
+                    <ul class="policy">
+                        <li><a href="#">회사소개</a></li>
+                        <li><a href="#">이용약관</a></li>
+                        <li><a href="#">개인정보취급방침</a></li>
+                        <li><a href="#">제휴문의</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-2 col-xs-offset-6">
+                    <ul class="sns">
+                        <li class="facebook"><a href="#"><img src="/design/brapra/images/common/ico_facebook.png" alt="페이스북"></a></li>
+                        <li class="nblog"><a href="#"><img src="/design/brapra/images/common/ico_nblog.png" alt="블로그"></a></li>
+                        <li class="kakaostory"><a href="#"><img src="/design/brapra/images/common/ico_kakaostory.png" alt="카카오스토리"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-7">
+                <section class="company">
+                    <h5>엠코르셋(주)</h5>
+                    <dl>
+                        <dt>주소</dt>
+                        <dd><address>서울시 강남구 언주로 151길 7 주성빌딩 2-3층</address></dd>
+                        <dt>전화번호</dt>
+                        <dd><span class="num">1544-6101</span></dd>
+                        <dt>팩스번호</dt>
+                        <dd><span class="num">02-6907-8867</span></dd>
+                    </dl>
+                    <dl>
+                        <dt>사업자등록번호</dt>
+                        <dd>109-81-59281</dd>
+                        <dt>통신판매업신고</dt>
+                        <dd>강남-5526호 <a class="check" href="#" target="_blank">[사업자정보확인]</a></dd>
+                    </dl>
+                    <dl>
+                        <dt>대표이사</dt>
+                        <dd>문영우</dd>
+                        <dt>개인정보관리 책임자</dt>
+                        <dd>김진국</dd>
+                    </dl>
+                </section>
+            </div>
+            <div class="col-xs-3 col-xs-offset-2">
+                <section class="escrow">
+                    <h5>구매안전(에스크로) 서비스</h5>
+                    <p>
+                        고객님은 안전거래를 위해 현금 등으로 결제 시 저희
+                        쇼핑몰에  가입한 KCP의  구매안전서비스를 이용
+                        하실  수 있습니다. <a class="check" href="#" target="_blank">가입사실 확인하기</a>
+                    </p>
+                </section>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <p class="copyright">
+                    COPYRIGHT (C) 2010-2016 MCORSET. ALL RIGHTS RESERVED.
+                    <a href="http://www.movementk.com" target="_blank">SITE BY MOVEMENT K GROUP</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
