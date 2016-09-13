cb404ca641b9846394737bfbb20c5dff4de7909c
kyh5392 <kyh90495392@gmail.com>
2016-09-13 19:12:04 +0900

    [mobile] 메인 퍼블리싱 작업중...

--- mobile/index.php
+++ mobile/index.php
@@ -26,7 +26,7 @@
         </header>
         <nav id="primary-menu">
             <ul>
-                <li class="active"><a href="#">NEW 5%</a></li>
+                <li><a href="#">NEW 5%</a></li>
                 <li><a href="#">BEST ITEM</a></li>
                 <li><a href="#">이달의 쿠폰</a></li>
                 <li><a href="#">UP TO 80%</a></li>
@@ -54,7 +54,121 @@
             </div>
         </main>
         <footer id="footer">
-            
+            <nav class="banner">
+                <a href="#">
+                    <img src="/design/brapra/mobile/images/main/footer_bg01.png" class="img-responsive" alt="푸터 배너 이미지">
+                </a>
+            </nav>
+            <div class="cs-center">
+                <h3>CS CENTER</h3>
+                <p class="cs-img">
+                    <a href="tel:1455-6101">
+                        <img src="/design/brapra/mobile/images/main/coll_center.png" alt="상담 센터">
+                    </a>
+                </p>
+                <ul class="cs-info">
+                    <li>
+                        <dl>
+                            <dt>평일</dt>
+                            <dd>10:00~17:00</dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>점심시간</dt>
+                            <dd>11:30~13:00</dd>
+                        </dl>
+                    </li>
+                </ul>
+                <p class="attention">주말/공휴일은 휴무입니다.</p>
+                <div class="quick-menu">
+                    <div class="container">
+                        <ul class="row">
+                            <li class="col-xs-3">
+                                <a href="#">베스트 리뷰</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">회원등급혜택</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">자주 묻는 질문</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">상품 Q&amp;A</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">장바구니</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">배송조회</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">마이페이지</a>
+                            </li>
+                            <li class="col-xs-3">
+                                <a href="#">1:1 문의</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="company-info">
+                <div class="notice">
+                    <dl>
+                        <dt>NOTICE</dt>
+                        <dd><a href="#">[공지] 2016년 5월 황금연휴 휴무안내</a></dd>
+                    </dl>
+                </div>
+                <div class="company">
+                    <ul class="terms">
+                        <li><a href="#">회사소개</a></li>
+                        <li><a href="#">이용약관</a></li>
+                        <li><a href="#">개인정보취급방침</a></li>
+                        <li><a href="#">제휴문의</a></li>
+                    </ul>
+                    <ul class="information">
+                        <li>
+                            <dl>
+                                <dt>상호</dt>
+                                <dd><i class="mcorset">엠코르셋(주)</i></dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>대표명</dt>
+                                <dd>문영우</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>주소</dt>
+                                <dd>서울시 강남구 언주로 151길 7 주성빌딩 2-3층</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                    <ul class="information">
+                        <li>
+                            <dl>
+                                <dt>전화번호</dt>
+                                <dd><a href="tel:15773622">1577-3622</a></dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>팩스번호</dt>
+                                <dd>02-6907-8867 </dd>
+                            </dl>
+                        </li>
+
+                        <li>
+                            <dl>
+                                <dt></dt>
+                                <dd></dd>
+                            </dl>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="/design/brapra/js/bootstrap.min.js"></script>
